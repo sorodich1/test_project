816fd21106719a326f48cde5e618a81f18a31c6e
sorodich1 <sorodich@gmail.com>
2022-05-30 08:53:36 +0300

    Комментарии

--- test_project/test_project/Program.cs
+++ test_project/test_project/Program.cs
@@ -1,21 +1,7 @@
 ﻿using System.Diagnostics;
-using System.Reflection;
 
 namespace test_project
 {
-    abstract class Inicialisation
-    {
-        public string One;
-        public int Age;
-        public double Money;
-        public Inicialisation(string one, int age, double money)
-        {
-            One = one;
-            Age = age;
-            Money = money;
-        }
-        public abstract void Abstract();
-    }
     class Program
     {
         public static void Analises()
@@ -23,7 +9,7 @@ namespace test_project
             string path = @"E:\Anna.txt";
             string TextString = ".,?!-/*”“'\"";
             string FileContents = File.ReadAllText(path);
-            
+            //Подсчёт колличества всевозможных знаков в тексте
             foreach (char c in TextString)
             {
                 FileContents = FileContents.Replace(c, ' ');
@@ -31,6 +17,7 @@ namespace test_project
             FileContents = FileContents.Replace(Environment.NewLine, " ");
             string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             Dictionary<string, int> map = new Dictionary<string, int>();
+            //Подсчёт колличества слов
             foreach (string word in Words.Select(x => x.ToLower()))
             {
                 if (map.TryGetValue(word, out int c))
@@ -46,48 +33,47 @@ namespace test_project
             Console.WriteLine(map);
             List<Tuple<int, string>> Stats = map.Select(x => new Tuple<int, string>(x.Value, x.Key)).ToList();
             Stats.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+            //Сбор статистики
             foreach (Tuple<int, string> pair in Stats)
             {
                 File.AppendAllText(@"E:\stats.txt", pair.Item2 + " " + pair.Item1 + Environment.NewLine);
+                return;
             }
         }
         static public void Main(string[] args)
         {
-            //Console.WriteLine("Основной поток запущен");
-            //Stopwatch stopwatch = new Stopwatch();
-            //stopwatch.Start();
-            //Console.WriteLine("Анализ текста запущен паралельно");
-            //Task task = new Task(Analises);
-            //task.Start();
-            //stopwatch.Stop();
-            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
-            //for (int i = 0; i < 60; i++)
-            //{
-            //    Console.Write(".");
-            //    Thread.Sleep(100);
-            //}
-            //Console.WriteLine("\r\nНажмите любую клавишу для продолжения");
-            //GC.Collect();
-            //Console.Read();
+            Console.WriteLine("Основной поток запущен");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Анализ текста запущен паралельно");
+            Task task = new Task(Analises);
+            task.Start();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("\r\nНажмите любую клавишу для продолжения");
+            //Очистка мусора и пауза для разграничения двух методов
+            GC.Collect();
+            Console.Read();
 
 
-            //Console.WriteLine("Анализ текста запущен однопоточно");
-            //stopwatch.Start();
-            //Program program = new Program();
-            //Program.Analises();
-            //stopwatch.Stop();
-            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
-            //for (int i = 0; i < 60; i++)
-            //{
-            //    Console.Write(".");
-            //    Thread.Sleep(100);
-            //}
-            //Console.WriteLine("\r\nРабота программы завершена");
-            //Console.ReadLine();
-
-            Type type = typeof(Inicialisation);
-            ConstructorInfo? info = type.GetConstructor(new Type[] { typeof(string), typeof(int), typeof(double) });
-            object inicialisation = info.Invoke(new object[] { "Test", 20, 25.5  });
+            Console.WriteLine("Анализ текста запущен однопоточно");
+            stopwatch.Start();
+            Program program = new Program();
+            Program.Analises();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("\r\nРабота программы завершена");
+            Console.ReadLine();
         }
     }
 }
